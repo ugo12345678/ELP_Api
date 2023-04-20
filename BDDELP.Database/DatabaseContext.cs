4dd2c99cf54335a6b602aa3d688d0c187225ad24
Ugo Marin <ugo.marin@kodeva.fr>
2023-04-20 14:30:25 +0200

    Ajoutez des fichiers projet.

---
+++ BDDELP.Database/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using BDDELP.Database.EntityModels;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BDDELP.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        { }
+
+        public DbSet<Utilisateur> Utilisateur { get; set; }
+    }
+}
---
+++ BDDELP.Database/EntityModels/Utilisateur.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDDELP.Database.EntityModels
+{
+    public class Utilisateur
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
